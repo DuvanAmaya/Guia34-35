d8d19514d1b09b56047ef55025d09479ee23339d
Duvan Camilo Amaya Lopez <duvan.amayalopez@unicolombo.edu.co>
2024-04-20 14:17:04 -0500

    Guia 34 y 35

---
+++ src/paquetePrincipal/panelInterno.java
@@ -0,0 +1,16 @@
+
+package paquetePrincipal;
+
+import javax.swing.JInternalFrame;
+
+/**
+ *
+ * @author HP
+ */
+class panelInterno {
+
+    static JInternalFrame[] getAllFrames() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+}
